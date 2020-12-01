de72bb6047ac499f65411c6d8d3ed12065b4d1cd
Ivan <1095036273@qq.com>
2020-12-01 11:23:53 +0800

    小程序更新重启

--- src/app.js
+++ src/app.js
@@ -1,15 +1,45 @@
 import { Component } from 'nervjs'
+import Taro from '@tarojs/taro'
 import './app.scss'
 
 class App extends Component {
+  componentDidMount() {}
 
-  componentDidMount() { }
+  componentDidShow() {
+    if (Taro.canIUse('getUpdateManager')) {
+      const updateManager = Taro.getUpdateManager()
+      updateManager.onCheckForUpdate(function(res) {
+        // 请求完新版本信息的回调
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function() {
+            Taro.showModal({
+              showCancel: false,
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启应用？',
+              success: response => {
+                // res: {errMsg: "showModal: ok", cancel: false, confirm: true}
+                if (response.confirm) {
+                  // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                  updateManager.applyUpdate()
+                }
+              },
+            })
+          })
+          updateManager.onUpdateFailed(function() {
+            // 新的版本下载失败
+            Taro.showModal({
+              title: '已经有新版本了哟~',
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~',
+            })
+          })
+        }
+      })
+    }
+  }
 
-  componentDidShow() { }
+  componentDidHide() {}
 
-  componentDidHide() { }
-
-  componentDidCatchError() { }
+  componentDidCatchError() {}
 
   // this.props.children 是将要会渲染的页面
   render() {
